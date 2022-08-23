9a407c0261406001b3d0cc298024262192683a92
juliovianadev <juliovianadev@gmail.com>
2022-08-23 19:27:04 -0300

    mudando as direções

--- snakepronto.py
+++ snakepronto.py
@@ -44,3 +44,35 @@ def show_score(choice, color, font, size):
     # displaying text
     game_window.blit(score_surface, score_rect)
  # meu primeiro código
+def game_over():
+    my_font = pygame.font.SysFont('times new roman',50)
+    game_over_surface = my_font.render(
+         'Sua pontuação: ' + str(score), True, red
+    )
+    game_over_rect = game_over_surface.get_rect()
+    game_over_rect.midtop = (window_x/2,window_y/4)
+    game_over_surface.blit(game_over_surface, game_over_rect)
+    pygame.display.flip()
+    time.sleep(2)
+    pygame.quit()
+    quit()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                change_to = 'UP'
+            if event.key == pygame.K_DOWN:
+                change_to = 'DOWN'
+            if event.key == pygame.K_LEFT:
+                change_to = 'LEFT'
+            if event.key == pygame.K_RIGHT:
+                change_to = 'RIGHT'
+    if change_to == 'UP' and direction != 'DOWN':
+        direction = 'UP' 
+    if change_to == 'DOWN' and direction != 'UP':
+      direction = 'DOWN'
+    if change_to == 'LEFT' and direction != 'RIGHT':
+        direction = 'LEFT'
+    if change_to == 'RIGHT' and direction != 'LEFT':
+        direction = 'RIGHT'   
+   
